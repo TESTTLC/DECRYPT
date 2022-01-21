7596970dc38490ea5e05b0d7f72b7de51cf31e73
Leonard <leontech@Leonards-MacBook-Pro.local>
2022-01-21 18:18:14 +0200

    Changed header login function

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,12 +6,76 @@ import GlowingButton from "./GlowingButton";
 import { routes } from "../utils/routes";
 import { Link } from "react-router-dom";
 
-// interface Props {
-//   connectWallet: () => void;
-//   disconnectWallet: () => void;
-//   account?: string;
-//   isMobileSize: boolean;
-// }
+interface LoginButtonProps {
+  connectWallet: () => void;
+  disconnectWallet: () => void;
+  account?: string;
+  isMobileDevice: boolean;
+}
+const metamaskAppDeepLink =
+  "https://metamask.app.link/dapp/app-theluxurybank-com-hrcup.ondigitalocean.app";
+// const metamaskAppDeepLink = "https://metamask.app.link/dapp/192.168.1.2:3000";
+
+const LoginButton: React.FC<LoginButtonProps> = ({
+  isMobileDevice,
+  account,
+  connectWallet,
+  disconnectWallet,
+}) => {
+  if (isMobileDevice) {
+    if (!window.ethereum) {
+      return (
+        <a href={metamaskAppDeepLink}>
+          <button className="group-hover:text-gray-100 h-8 relative px-7 py-2 bg-black leading-none flex items-center divide-x divide-gray-600">
+            {/* Connect to MetaMask */}
+            <span className="hover:text-gray-100 font-poppins py-4 text-sm text-indigo-400 transition duration-200">
+              {account
+                ? `Disconnect ${account.slice(0, 4)}...${account.slice(
+                    account.length - 4,
+                    account.length
+                  )}`
+                : "Connect wallet"}{" "}
+            </span>
+          </button>
+        </a>
+      );
+    } else {
+      return (
+        <button
+          onClick={account ? disconnectWallet : connectWallet}
+          className="group-hover:text-gray-100 h-8 relative px-7 py-2 bg-black leading-none flex items-center divide-x divide-gray-600"
+        >
+          {/* Connect to MetaMask */}
+          <span className="hover:text-gray-100 font-poppins py-4 text-sm text-indigo-400 transition duration-200">
+            {account
+              ? `Disconnect ${account.slice(0, 4)}...${account.slice(
+                  account.length - 4,
+                  account.length
+                )}`
+              : "Connect wallet"}{" "}
+          </span>
+        </button>
+      );
+    }
+  } else {
+    return (
+      <button
+        onClick={account ? disconnectWallet : connectWallet}
+        // className="h-10 bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-3xl"
+        className="group-hover:text-gray-100 h-8 relative px-7 py-2 bg-black leading-none flex items-center divide-x divide-gray-600"
+      >
+        <span className="hover:text-gray-100 font-poppins py-4 text-sm text-indigo-400 transition duration-200">
+          {account
+            ? `Disconnect ${account.slice(0, 4)}...${account.slice(
+                account.length - 4,
+                account.length
+              )}`
+            : "Connect wallet"}{" "}
+        </span>
+      </button>
+    );
+  }
+};
 
 const Header: React.FC = () => {
   const {
@@ -30,9 +94,9 @@ const Header: React.FC = () => {
   // const metamaskAppDeepLink = useRef(
   //   "https://metamask.app.link/dapp/decryption.com"
   // ).current;
-  const metamaskAppDeepLink = useRef(
-    "https://metamask.app.link/dapp/app-theluxurybank-com-hrcup.ondigitalocean.app/"
-  ).current;
+  // const metamaskAppDeepLink = useRef(
+  //   "https://metamask.app.link/dapp/app-theluxurybank-com-hrcup.ondigitalocean.app"
+  // ).current;
 
   return (
     <div className="w-full z-30 flex flex-wrap justify-between xs:justify-center sm:justify-center items-center px-10 py-5 bg-customBlue-800">
@@ -86,36 +150,12 @@ const Header: React.FC = () => {
         <div className="items-center justify-center mx-2">
           <div className="relative">
             <div className="absolute -inset-0 bg-gradient-to-r from-green-400 to-blue-600 rounded-lg blur-sm opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt "></div>
-            {isMobileDevice ? (
-              <a href={metamaskAppDeepLink}>
-                <button className="group-hover:text-gray-100 h-8 relative px-7 py-2 bg-black leading-none flex items-center divide-x divide-gray-600">
-                  {/* Connect to MetaMask */}
-                  <span className="hover:text-gray-100 font-poppins py-4 text-sm text-indigo-400 transition duration-200">
-                    {account
-                      ? `Disconnect ${account.slice(0, 4)}...${account.slice(
-                          account.length - 4,
-                          account.length
-                        )}`
-                      : "Connect wallet"}{" "}
-                  </span>
-                </button>
-              </a>
-            ) : (
-              <button
-                onClick={account ? disconnectWallet : connectWallet}
-                // className="h-10 bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-3xl"
-                className="group-hover:text-gray-100 h-8 relative px-7 py-2 bg-black leading-none flex items-center divide-x divide-gray-600"
-              >
-                <span className="hover:text-gray-100 font-poppins py-4 text-sm text-indigo-400 transition duration-200">
-                  {account
-                    ? `Disconnect ${account.slice(0, 4)}...${account.slice(
-                        account.length - 4,
-                        account.length
-                      )}`
-                    : "Connect wallet"}{" "}
-                </span>
-              </button>
-            )}
+            <LoginButton
+              account={account}
+              connectWallet={connectWallet}
+              disconnectWallet={disconnectWallet}
+              isMobileDevice={isMobileDevice}
+            />
           </div>
         </div>
       </div>
