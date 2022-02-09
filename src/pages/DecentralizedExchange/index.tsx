4f443626bb17864201c3467d238f4f4a387423f6
Leonard <leonard@Leonards-MacBook-Pro-2.local>
2022-02-09 02:17:38 +0200

    Decentralized exchange

--- src/pages/DecentralizedExchange/index.tsx
+++ src/pages/DecentralizedExchange/index.tsx
@@ -147,114 +147,111 @@ const DecentralizedExchange: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-1 justify-center">
-      <p className="text-white font-poppins text-xl font-bold">Coming Soon</p>
-    </div>
-    // <div className="flex flex-col flex-1 items-center">
-    //   <div className="flex flex-col">
-    //     <div className="w-[38rem] xs:w-[22rem] mt-10 text-sm mb-4">
-    //       <p className="font-poppins font-bold text-gray-300 text-3xl">
-    //         Trade Coin In An Instant
-    //       </p>
-    //     </div>
-    //     <div className="flex justify-between">
-    //       <div className="flex">
-    //         <p className="text-green-400 font-poppins font-semibold text-lg mb-4">
-    //           {usdtBalance} USDT
-    //         </p>
-    //         <p className="text-white font-poppins font-semibold text-lg mb-4">
-    //           &nbsp;available on BSC
-    //         </p>
-    //       </div>
-    //     </div>
-    //     {chainErrorMessage && (
-    //       <p className="mb-2 font-poppins text-red-400">{chainErrorMessage}</p>
-    //     )}
-    //     <div className="relative items-center w-[38rem] xs:w-[22rem] h-[25rem] px-8 py-8 xs:px-4 rounded-lg bg-black bg-opacity-60">
-    //       <div className="relative flex bg-black bg-opacity-60 w-full h-20 rounded-lg pt-1 px-6 items-center">
-    //         <div className="flex w-1/2 flex-col h-full">
-    //           <p className="text-gray-400 font-medium font-poppins text-sm">
-    //             From
-    //           </p>
-    //           <input
-    //             className="w-full h-2/3 text-lg pt-2 bg-transparent font-poppins text-white focus:outline-none"
-    //             onChange={handleAmountChange}
-    //             value={usdtAmountToSwap}
-    //             type="number"
-    //           ></input>
-    //         </div>
-    //         {/* <TokensModal tokens={localModalTokens} type="from" /> */}
-    //         <div className="flex w-1/2 justify-end items-center mt-4">
-    //           <img
-    //             className="text-white font-poppins w-4 h-4 mr-2 object-cover "
-    //             src={tetherImage}
-    //           />
-    //           <p className="font-poppins text-md text-white">USDT</p>
-    //         </div>
-    //       </div>
-    //       <div className="w-full flex items-center justify-center h-14">
-    //         <FaArrowCircleDown
-    //           className="h-6 w-6 bg-transparent self-center "
-    //           color="gray"
-    //         />
-    //       </div>
-    //       <div className="relative flex bg-black bg-opacity-60 w-full h-20 rounded-lg pt-1 px-6 items-center">
-    //         <div className="flex w-1/2 flex-col h-full">
-    //           <p className="text-gray-400 font-medium font-poppins text-sm">
-    //             To
-    //           </p>
-    //           <input
-    //             className="w-full h-2/3 text-lg pt-2 bg-transparent font-poppins text-white focus:outline-none"
-    //             type="number"
-    //             disabled
-    //             value={usdtAmountToSwap * 25}
-    //           ></input>
-    //         </div>
-    //         {/* <TokensModal tokens={[]} type="to" /> */}
-    //         <div className="flex w-1/2 justify-end items-center mt-4">
-    //           <img
-    //             className="text-white font-poppins w-6 h-6 mr-2 object-cover "
-    //             src={tlchainImage}
-    //           />
-    //           <p className="font-poppins text-md text-white">TLC</p>
-    //         </div>
-    //       </div>
-    //       <p className="font-poppins text-gray-300 mt-2">
-    //         Exchange Rate: 1 USDT ≃ 25 TLC
-    //       </p>
-    //       <p className="mb-2 font-poppins text-red-400">
-    //         Please note that there's a minim of {minimumAmount} USDT (BEP20) per
-    //         swap
-    //       </p>
-    //       <button
-    //         onClick={() => {
-    //           if (usdtAmountToSwap >= minimumAmount) {
-    //             send();
-    //           }
-    //         }}
-    //         className="mt-2 flex w-full h-14 text-white text-md font-poppins items-center justify-center bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl font-medium rounded-lg px-5 text-center"
-    //         disabled={isLoading || usdtAmountToSwap < minimumAmount}
-    //       >
-    //         {isLoading ? (
-    //           <>
-    //             {" "}
-    //             Exchange in progress &nbsp;
-    //             <TailSpin color="#fff" height={18} width={18} />
-    //           </>
-    //         ) : (
-    //           "Exchange"
-    //         )}
-    //       </button>
-    //     </div>
-    //   </div>
+    <div className="flex flex-col flex-1 items-center">
+      <div className="flex flex-col">
+        <div className="w-[38rem] xs:w-[22rem] mt-10 text-sm mb-4">
+          <p className="font-poppins font-bold text-gray-300 text-3xl">
+            Trade Coin In An Instant
+          </p>
+        </div>
+        <div className="flex justify-between">
+          <div className="flex">
+            <p className="text-green-400 font-poppins font-semibold text-lg mb-4">
+              {usdtBalance} USDT
+            </p>
+            <p className="text-white font-poppins font-semibold text-lg mb-4">
+              &nbsp;available on BSC
+            </p>
+          </div>
+        </div>
+        {chainErrorMessage && (
+          <p className="mb-2 font-poppins text-red-400">{chainErrorMessage}</p>
+        )}
+        <div className="relative items-center w-[38rem] xs:w-[22rem] h-[25rem] px-8 py-8 xs:px-4 rounded-lg bg-black bg-opacity-60">
+          <div className="relative flex bg-black bg-opacity-60 w-full h-20 rounded-lg pt-1 px-6 items-center">
+            <div className="flex w-1/2 flex-col h-full">
+              <p className="text-gray-400 font-medium font-poppins text-sm">
+                From
+              </p>
+              <input
+                className="w-full h-2/3 text-lg pt-2 bg-transparent font-poppins text-white focus:outline-none"
+                onChange={handleAmountChange}
+                value={usdtAmountToSwap}
+                type="number"
+              ></input>
+            </div>
+            {/* <TokensModal tokens={localModalTokens} type="from" /> */}
+            <div className="flex w-1/2 justify-end items-center mt-4">
+              <img
+                className="text-white font-poppins w-4 h-4 mr-2 object-cover "
+                src={tetherImage}
+              />
+              <p className="font-poppins text-md text-white">USDT</p>
+            </div>
+          </div>
+          <div className="w-full flex items-center justify-center h-14">
+            <FaArrowCircleDown
+              className="h-6 w-6 bg-transparent self-center "
+              color="gray"
+            />
+          </div>
+          <div className="relative flex bg-black bg-opacity-60 w-full h-20 rounded-lg pt-1 px-6 items-center">
+            <div className="flex w-1/2 flex-col h-full">
+              <p className="text-gray-400 font-medium font-poppins text-sm">
+                To
+              </p>
+              <input
+                className="w-full h-2/3 text-lg pt-2 bg-transparent font-poppins text-white focus:outline-none"
+                type="number"
+                disabled
+                value={usdtAmountToSwap * 25}
+              ></input>
+            </div>
+            {/* <TokensModal tokens={[]} type="to" /> */}
+            <div className="flex w-1/2 justify-end items-center mt-4">
+              <img
+                className="text-white font-poppins w-6 h-6 mr-2 object-cover "
+                src={tlchainImage}
+              />
+              <p className="font-poppins text-md text-white">TLC</p>
+            </div>
+          </div>
+          <p className="font-poppins text-gray-300 mt-2">
+            Exchange Rate: 1 USDT ≃ 25 TLC
+          </p>
+          <p className="mb-2 font-poppins text-red-400">
+            Please note that there's a minim of {minimumAmount} USDT (BEP20) per
+            swap
+          </p>
+          <button
+            onClick={() => {
+              if (usdtAmountToSwap >= minimumAmount) {
+                send();
+              }
+            }}
+            className="mt-2 flex w-full h-14 text-white text-md font-poppins items-center justify-center bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl font-medium rounded-lg px-5 text-center"
+            disabled={isLoading || usdtAmountToSwap < minimumAmount}
+          >
+            {isLoading ? (
+              <>
+                {" "}
+                Exchange in progress &nbsp;
+                <TailSpin color="#fff" height={18} width={18} />
+              </>
+            ) : (
+              "Exchange"
+            )}
+          </button>
+        </div>
+      </div>
 
-    //   <div className="w-[38rem] xs:w-[22rem] mt-4 text-lg">
-    //     <p className="font-poppins text-gray-300">
-    //       It will take a minute. Please check your wallet to get{" "}
-    //       {usdtAmountToSwap * 25} TLC
-    //     </p>
-    //   </div>
-    // </div>
+      <div className="w-[38rem] xs:w-[22rem] mt-4 text-lg">
+        <p className="font-poppins text-gray-300">
+          It will take a minute. Please check your wallet to get{" "}
+          {usdtAmountToSwap * 25} TLC
+        </p>
+      </div>
+    </div>
   );
 };
 
