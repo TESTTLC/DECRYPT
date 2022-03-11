6cae8b89b0b849b8175a1c6cc6fc1a25a861059e
Leonard <leonard.busoi96@gmail.com>
2022-03-11 11:01:22 +0200

    added chart

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -331,6 +331,7 @@ import { StoreState } from 'src/utils/storeTypes';
 import dotenv from 'dotenv';
 import { changeChain } from 'src/utils/functions/MetaMask';
 import MarketTrendResults from 'src/assets/svg/MarketTrendResults';
+import LineChart from 'src/assets/svg/LineChart';
 
 import { useContracts } from '../../hooks/useContracts';
 import {
@@ -583,10 +584,10 @@ const Dashboard: React.FC = () => {
       <div className="flex space-x-12 mt-4">
         <div className="h-60">
           <p className="font-bold text-2xl">Good morning, Leonard!</p>
-          <p className="text-gray-500 text-[0.65rem] mb-4">
+          <p className="text-gray-500 text-[0.65rem] mt-4">
             Now you have full control over your financial instruments
           </p>
-          <div className="bg-customBlue-800 rounded-lg py-4">
+          <div className="bg-customBlue-800 rounded-lg py-4 mt-16">
             <div className="px-8">
               <p>Today</p>
               <p className="text-gray-500 text-xs">Market trend</p>
@@ -616,27 +617,12 @@ const Dashboard: React.FC = () => {
           <p className="text-xl">Market Analysis TLC</p>
 
           <div className="bg-customBlue-800 rounded-lg py-4 mt-2">
-            <div className="px-8">
-              <p>Today</p>
-              <p className="text-gray-500 text-xs">Market trend</p>
-              <div className="flex space-x-2 text-lg">
-                <p className="text-pink-500">68,5%</p>
-                <p className="text-[#5EFF5A] bg-green-900 rounded-r-full px-4">
-                  12%
-                </p>
+            <LineChart />
+            <div className="bg-gray-800 rounded-lg">
+              <div className="flex">
+                <div className="w-2 h-2 rounded-full bg-indigo-700" />
+                <p>Communication Platforms</p>
               </div>
-              <p className="text-gray-500 text-[0.55rem] mt-2">
-                Results provided by the BERT AND GPT3 programs.
-              </p>
-              <p className="text-gray-500 text-[0.55rem]">
-                There results are worthless, do not act on them.
-              </p>
-            </div>
-            <MarketTrendResults />
-            <div className="h-[0.1rem] w-full bg-gray-700 my-4"></div>
-            <div className="flex">
-              <div className="w-2 h-2 rounded-full bg-indigo-700" />
-              <p>Communication Platforms</p>
             </div>
           </div>
         </div>
