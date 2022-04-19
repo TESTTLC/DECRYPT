675d95798c5cd67eb1a088908e04640ef46ba538
Leonard <leonard.busoi96@gmail.com>
2022-04-20 00:23:24 +0300

    fixed bug

--- src/utils/globals.tsx
+++ src/utils/globals.tsx
@@ -7,7 +7,7 @@ import maticBridgeImage from '../assets/images/matic-bridge.png';
 
 import { Project } from './types';
 
-export const MarketplaceAddress = '0xaF828bd31483cF8F5515ea44D1A2904f32422A51';
+export const marketplaceAddress = '0xaF828bd31483cF8F5515ea44D1A2904f32422A51';
 
 // /** Mainnet */
 export const TLXTokenContractAddress =
