11a3238f3e1851c975db6e21e821897537a1c818
Leonard <leonard.busoi96@gmail.com>
2022-10-23 23:30:42 +0300

    removed unfreeze button

--- src/pages/Staking/components/StakeCoin.tsx
+++ src/pages/Staking/components/StakeCoin.tsx
@@ -577,7 +577,7 @@ const StakeCoin: React.FC = () => {
           </div>
         </div>
       </div>
-      {(coinTag === 'TLX' || coinTag === 'LSO') && walletAddress && (
+      {/* {(coinTag === 'TLX' || coinTag === 'LSO') && walletAddress && (
         <div className="flex w-full items-center justify-center mt-4">
           <button
             className="flex h-10 mt-2 text-sm items-center justify-center bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl font-medium rounded-lg px-5 text-center"
@@ -586,7 +586,7 @@ const StakeCoin: React.FC = () => {
             Unfreeze {coinTag}
           </button>
         </div>
-      )}
+      )} */}
 
       {renderTable()}
     </div>
